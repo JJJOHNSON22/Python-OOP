2b2e2f31d0a2a3ae492412e6303a8f01e551ee51
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-02-24 13:05:37 -0500

    got rid of syntax errors

--- test_even.py
+++ test_even.py
@@ -4,28 +4,28 @@ import unittest
 # this is what we are running our test on
 def isEven(n):
     if n % 2 == 0:
-       return True
+        return True
     else:
-       return False
+        return False
 # our "unit tests"
 # initialized by creating a class that inherits from unittest.TestCase
 class IsEvenTests(unittest.TestCase):
     # each method in this class is a test to be run
     def testTwo(self):
         self.assertEqual(isEven(2), True)
-        # another way to write above is
+    # another way to write above is
         self.assertTrue(isEven(2))
     def testThree(self):
         self.assertEqual(isEven(3), False)
         # another way to write above is
         self.assertFalse(isEven(3))
     # any task you want run before any method above is executed, put them in the setUp method
-    def setUp(self):
+    def setUp(self):
         # add the setUp tasks
         print("running setUp")
     # any task you want run after the tests are executed, put them in the tearDown method
     def tearDown(self):
-        # add the tearDown tasks
+    # add the tearDown tasks
         print("running tearDown tasks")
 if __name__ == '__main__':
     unittest.main() # this runs our tests
