55286cf33cf2d93aebf094d3347b2fb893a2cefd
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-02-25 09:26:58 -0500

    updated with TDD

--- math_dojo.py
+++ math_dojo.py
@@ -1,4 +1,6 @@
-class MathDojo:
+import unittest
+
+class mathDojo:
     def __init__(self):
         self.result = 0
     def add(self, num, *nums):
@@ -12,7 +14,23 @@ class MathDojo:
         self.result -= num
         return self
 
-md = MathDojo()
-x = md.add(2).add(2,5,1).subtract(3,2).add(2,3).subtract(1,3,4).subtract(1).result
-print(x)
+class mathDojoTest(unittest.TestCase):
+    def setUp(self):
+        md = mathDojo()
+        self.x1 = md.add(2).add(2,5,1).subtract(3,2).add(2,3).subtract(1,3,4).subtract(1).result
+        self.x2 = md.add(2).result
+        self.x3 = md.subtract(1).result
+    def testEqual(self):
+        self.assertEqual(self.x1, 1)
+    def testAddition(self):
+        self.assertEqual(self.x2, 3)
+    def testSubtract(self):
+        self.assertIsNotNone(self.x3, 2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# md = mathDojo()
+# x = md.add(2).add(2,5,1).subtract(3,2).add(2,3).subtract(1,3,4).subtract(1).result
+# print(x)
 # should output 1
